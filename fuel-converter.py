d120d65772e9764a880609337f508ac9e7d826e4
Alex Friedman <106344806+friedmanalexg@users.noreply.github.com>
2024-01-29 18:33:30 -0500

    touch fuel-converter.py

---
+++ fuel-converter.py
@@ -0,0 +1,7 @@
+print("Hello Rachel. You are very pretty today.")
+
+my_price = float(input("What's the price of gas?"))
+print("Okay, ",my_price,".")
+my_loc = input("Are you in the USA or Canada? Type U or C.").upper()
+#if my_loc doesn't equal 'U' or 'C', ask again for input.
+#if my_loc = U, my_price 
